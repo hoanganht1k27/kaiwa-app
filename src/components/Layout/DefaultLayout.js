620a43d6212d135dfc39a3646bed51e66858ef32
phuctong2002 <vanphuc15092002@gmail.com>
2022-10-08 15:32:08 +0700

    sort

--- src/components/Layout/DefaultLayout.js
+++ src/components/Layout/DefaultLayout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from "~/assets/css/default.module.css"
 import logo from '~/assets/images/image1.svg';
+import defaultAvatar from '~/assets/images/DefaultAvatar.svg'
 export default function DefaultLayout({ children }) {
   return (
     <div>
@@ -9,15 +10,16 @@ export default function DefaultLayout({ children }) {
         <div className='col-span-3'>
           <img src={logo } alt="text" className={style.logo} />
         </div>
-        <div className={`col-span-6 ${style.containerLink}`}>
+        <div className={`col-span-4 ${style.containerLink}`}>
           <NavLink className={style.link} to="/">Home</NavLink>
           <NavLink className={style.link} to="/">Video</NavLink>
           <NavLink className={style.link} to="/">Record</NavLink>
         </div>
-        <div className={`col-span-3 ${style.containerIcon}`}>
+        <div className={`col-span-5 ${style.containerIcon}`}>
           <i className={`fa-solid fa-music ${style.icon}`}></i>
           <i  className={`fa-solid fa-bell ${style.icon}`} ></i>
-          <i className={`fa-solid fa-user ${style.icon}`} ></i>
+          {/* <i className={`fa-solid fa-user ${style.icon}`} ></i> */}
+          <img src={defaultAvatar} alt="text" className={`${style.icon}`} />
         </div>
       </div>
       {children}
--- src/pages/Home/Record.js
+++ src/pages/Home/Record.js
@@ -1,16 +1,25 @@
 import {memo} from 'react';
 import style from '~/assets/css/record.module.css';
 function Record({records}){
-    console.log( records);
+    // console.log( records);
     return <div className={style.container}>
         {
-            records.map( (record, index)=>{
-                console.log( record);
-                return <div className={style.video}>
-                    <img alt='text' className={style.image} src='https://cdn.pixabay.com/photo/2014/10/09/13/14/video-481821_960_720.png'/>
+            records.map((record, index) => {
+                return <div key={index} className={style.video}>
+                    <img alt='text' className={style.image} src={record.url} />
+                    <div className='px-3'>
+                        <div className={`${style.description1} ${style.border}`}>
+                            <p className={style.level}>{record.level}</p>
+                            <p className={style.view}>{record.uploaded_at}</p>
+                        </div>
+                    </div>
                     <div className={style.description1}>
-                        <p className={style.level}>{record.name}</p>
-                        <p className={style.view}>{record.date} date</p>
+                        <p className={style.topic}>{record.topic}</p>
+                        <p className={`${style.topic} ${style.topic2}`}>{record.level}</p>
+                    </div>
+                    <div className={style.description1}>
+                        <p className={style.topic}><i className={`fa-solid fa-user ${style.iconWatching}`}> </i>{record.teacher_name}</p>
+                        <p className={`${style.topic} ${style.topic2}`}>{record.level}</p>
                     </div>
                 </div>
             })
